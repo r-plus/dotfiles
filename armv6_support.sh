f7cbbca1e82037ddd3f874b16a9034d8f2539e83
r-plus <anasthasia.r@gmail.com>
2014-03-15 14:57:35 +0900

    function

--- armv6_support.sh
+++ armv6_support.sh
@@ -4,19 +4,28 @@ Xcode='/Applications/Xcode.app'
 Xcode4='/Applications/Xcode4.4.1.app'
 # Xcode 4.4.1 download from https://developer.apple.com/downloads/index.action?q=xcode%204.4.1
 
+combine_armv6()
+{
+    if [ "$1" = "libclang_rt.ios.a" ]; then 
+        # Supposition: contain only one clang version.
+        cd $Xcode/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/*/lib/darwin/
+        armv6_object="$Xcode4/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/4.0/lib/darwin/libclang_rt.ios.a"
+    elif [ "$1" = "dylib1.o" ]; then
+        # Supposition: contain only one sdk.
+        cd $Xcode/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS*.sdk
+        armv6_object="$Xcode4/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS5.1.sdk/usr/lib/dylib1.o"
+    else
+        echo "Not support"
+        exit 1
+    fi
+    sudo mv $1 $1.latest
+    sudo lipo -thin armv6 -output $1.armv6 $armv6_object
+    sudo lipo -create -output $1 $1.armv6 $1.latest
+    [ $(lipo -info $1 | grep -wc armv6) -ne 0 ] && echo "$1 [ Success ]" || echo "$1 [ Failed ]"
+}
+
 ### libclang_rt.ios.a
-# Supposition: contain only one clang version.
-cd $Xcode/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/*/lib/darwin/
-sudo mv libclang_rt.ios.a libclang_rt.ios.latest.a
-sudo lipo -thin armv6 -output libclang_rt.ios.armv6.a $Xcode4/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/4.0/lib/darwin/libclang_rt.ios.a
-sudo lipo -create -output libclang_rt.ios.a libclang_rt.ios.armv6.a libclang_rt.ios.latest.a
-[ $(lipo -info libclang_rt.ios.a | grep -wc armv6) -ne 0 ] && echo "libclang_rt [ Success ]" || echo "libclang_rt [ Failed ]"
+combine_armv6 libclang_rt.ios.a
 
 ### dylib1.o
-# Supposition: contain only one sdk.
-#cd $(xcrun --show-sdk-path --sdk iphoneos)/usr/lib
-cd $Xcode/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS*.sdk
-sudo mv dylib1.o dylib1.latest.o
-sudo lipo -thin armv6 -output dylib1.armv6.o $Xcode4/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS5.1.sdk/usr/lib/dylib1.o
-sudo lipo -create -output dylib1.o dylib1.armv6.o dylib1.latest.o
-[ $(lipo -info dylib1.o | grep -wc armv6) -ne 0 ] && echo "dylib1.o [ Success ]" || echo "dylib1.o [ Failed ]"
+combine_armv6 dylib1.o
