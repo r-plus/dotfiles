bee504d65679d66d270c9945ed5dfb8b8e1bcdb2
r-plus <anasthasia.r@gmail.com>
2017-06-28 15:59:30 +0900

    Add eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "installedESLint": true,
+    "plugins": [
+        "standard",
+    ],
+    "rules": {
+        "no-console": "off",
+        "semi": "error"
+    }
+};
